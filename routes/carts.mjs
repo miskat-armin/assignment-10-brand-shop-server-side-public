122960130441c32b86ea61a108ffb31e975dade0
miskat-armin <miskatarmin123@gmail.com>
2023-10-20 19:12:52 +0600

    cart delete implemented

--- routes/carts.mjs
+++ routes/carts.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "../db/conn.mjs";
-
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
@@ -21,9 +21,29 @@ router.get('/:user', async(req, res) => {
 
 router.post('/addToCart', async (req, res) => {
     const collection = db.collection("carts")
-    collection.insertOne(req.body)
+    const params = req.body
+    collection.insertOne({
+        name: params.name,
+        image: params.image,
+        brand_name: params.brand_name,
+        price:params.price,
+        user: params.user,
+        rating: params.rating,
+        type: params.type
+    })
     .then(result => res.status(200).send(result))
     .catch( err => res.status(500).send({error:true, message: "Error while adding to cart"}))
+})
+
+
+router.delete('/delete/:id', async(req, res) => {
+    const collection = db.collection("carts")
+
+   collection.deleteOne({
+        _id: new ObjectId(req.params.id)
+    })
+    .then(result => res.status(200).send(result))
+    .catch(e => res.status(500).send({error:true, message:"Cannot delete cart item"}))
 })
 
 export default router
